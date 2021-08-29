d483c2639bae57e62bd6a148c22b635ba6669487
d2jvkpn <chenbin2018@protonmail.com>
2021-08-29 15:17:34 +0800

    ...

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ test -z "${changes}" || { echo "You have uncommitted changes!"; exit 1; }
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 go build -o psctl -ldflags="                 \
-  -X psctl/cmd.BuildBranch=${branch}        \
+  -X psctl/cmd.BuildBranch=${branch}         \
   -X psctl/cmd.BuildTime=$(date +'%FT%T%z')" \
   psctl.go
 
