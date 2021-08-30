e8e48612820ce35d60b3928ff5dbceb0b29821a8
d2jvkpn <chenbin2018@protonmail.com>
2021-08-30 10:42:57 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,15 @@ set -eu -o pipefail
 
 wd=$(pwd)
 
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 git add -A # include Untracked files for git diff
 test -z "$(git diff HEAD)" || { echo "You have uncommitted changes!"; exit 1; }
+
+test -z "$(git diff origin/$branch..HEAD --name-status)" ||
+{ echo "You have unpushed changes!"; exit 1; }
+
 
 go build -o psctl -ldflags="                 \
   -X psctl/cmd.BuildBranch="${branch}"       \
