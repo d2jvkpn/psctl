5ab3da9b9de790ea533bd4e7845c8519ef76c950
d2jvkpn <chenbin2018@protonmail.com>
2021-09-11 10:26:55 +0800

    ...

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ set -eu -o pipefail
 _wd=$(pwd)
 _path=$(dirname $0)
 
-
+####
 test -z "$(git status --short)" || { echo "You have uncommitted changes!"; exit 1; }
 
 b1=$(git rev-parse --abbrev-ref HEAD)
 test -z "$(git diff origin/$b1..HEAD --name-status)" ||
   { echo "You have unpushed commits!"; exit 1; }
 
-
+####
 branch=$b1
 
 go build -o psctl -ldflags="                 \
@@ -19,4 +19,5 @@ go build -o psctl -ldflags="                 \
   -X psctl/cmd.BuildTime=$(date +'%FT%T%z')" \
   psctl.go
 
+####
 ./psctl version
